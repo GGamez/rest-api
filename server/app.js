80bd18231aa6331421c92bd82488d2e8b1be84fd
Guillermo <ggamezcorcho@gmail.com>
2019-11-11 10:07:25 +0100

    Npm start listo

--- server/app.js
+++ server/app.js
@@ -302,7 +302,7 @@ app.post('/n-back', function(req, res) {
     });
 
     console.log(req.body)
-    usuario.insert((err, usuarioDB) => {
+    usuario.save((err, usuarioDB) => {
         if (err) {
             return res.status(400).json({
                 ok: false,
